984fab578ee45294a9c9d787da15a04851daa6b4
Nikappa57 <lorenz.gaudino@gmail.com>
2025-04-19 13:40:44 +0200

    apps and models init

---
+++ src/apps/core/models/CustomUser.py
@@ -0,0 +1,42 @@
+from decimal import Decimal
+from enum import Enum
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+
+class EconomicalLevel(Enum):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+
+
+class CustomUser(AbstractUser):
+    """"
+    User(
+        Email, 
+        first_name, 
+        last_name,
+        propic, 
+        credit, 
+        university,
+        economical level*)
+    f.k User[University] (= University[Name]
+    """
+
+    propic = models.ImageField(upload_to='profile_pics/',
+                               blank=True,
+                               null=True)
+    credit = models.DecimalField(max_digits=10,
+                                 decimal_places=2,
+                                 default=Decimal('0.00'))
+    economical_level = models.CharField(max_length=10,
+                                        choices=[(tag.name, tag.value)
+                                                 for tag in EconomicalLevel],
+                                        default=EconomicalLevel.LOW.name)
+    university = models.ForeignKey('University', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.email
---
+++ src/apps/core/models/University.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class University(models.Model):
+    """"
+    University(Name)
+    """
+
+    name = models.CharField(max_length=100, primary_key=True)
+
+    def __str__(self):
+        return self.name
